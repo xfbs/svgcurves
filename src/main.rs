918a32cc28c966a9511d021b382cc46c51a1ca2a
Patrick Elsen <pelsen@xfbs.net>
2018-10-15 19:28:00 +0200

    Adds command-line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+extern crate clap;
+extern crate svg;
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let args = App::new("SVG paths tool")
+        .version("0.1.0")
+        .author("Patrick Elsen <pelsen@xfbs.net>")
+        .about("Transforms SVG quadratic and cubic (bézier) curves.")
+        .arg(Arg::with_name("INPUT")
+             .help("Which file to process")
+             .required(true)
+             .index(1))
+        .arg(Arg::with_name("verbose")
+             .help("Enable verbose output")
+             .short("v")
+             .multiple(true))
+        .get_matches();
+
+    let input_file_name = args.value_of("INPUT").unwrap();
+
+    process(input_file_name)
+}
+
+fn process(file_name: &str) {
 }
